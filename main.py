70ec9a766f9931f06e96530474165c0f0b5e8309
C0dingTime <ntergalactic1@gmail.com>
2026-04-26 18:42:31 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main();
-    print ('Hello World')
-    print ('CS 110: Introduction to Programming Concepts and Methodologies')
-
+def main():
+    ret1 = 'Hello World'
+    ret2 = 'CSV09: Principles of Computing'
+    print (ret1)
+    print (ret2)
 if __name__ == ‘__main__’:
     main()
